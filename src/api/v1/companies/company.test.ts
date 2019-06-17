f2c8120adda518887002671b91cf54921c43ae4d
DESKTOP-G4GI154\iesty <iestyn02@gmail.com>
2019-06-17 08:17:23 +0100

    Modified GET test & updated package.json

--- src/api/v1/companies/company.test.ts
+++ src/api/v1/companies/company.test.ts
@@ -32,7 +32,7 @@ describe('Companies API', () => {
         .expect((res: any) => {
           if (!res.body.data) throw new Error('API: Expected data in response');
           console.warn(res.body.data.length);
-          if (res.body.data.length >= 3) throw new Error('API: Expected 3 companies in response');
+          if (res.body.data.length < 3) throw new Error('API: Expected 3 companies or more in response');
         })
         .expect('Content-Type', /json/)
         .expect(200, done);
